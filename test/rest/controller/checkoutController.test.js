2ae9cd7c281b5245ce9188748e9117c33df36673
JadsonCardoso <jadsonc190@gmail.com>
2025-09-20 10:32:24 -0300

    Alternacações e melhorias nos dados e DDT no teste controller

--- test/rest/controller/checkoutController.test.js
+++ test/rest/controller/checkoutController.test.js
@@ -73,49 +73,24 @@ describe('Checkout Controller', () => {
             expect(resposta.status).to.equal(200);
         });
 
-
-        it('Checkout via cartão sem preencher os dados do cartão, recebo 400', async () => {
-            const checkoutMock = sinon.stub(checkoutService, 'checkout');
-            checkoutMock.throws(new Error('Dados do cartão obrigatórios para pagamento com cartão'));
-
-            const resposta = await request(app)
-                .post('/api/checkout')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    items: [
-                        {
-                            "productId": 1,
-                            "quantity": 2
-                        }
-                    ],
-                    freight: 0,
-                    paymentMethod: "credit_card",
-                });
-
-            expect(resposta.status).to.equal(400);
-            expect(resposta.body).to.have.property('error', 'Dados do cartão obrigatórios para pagamento com cartão');
-        });
-
-        it('Checkout via boleto com produto não cadastrado, recebo 400', async () => {
-            const calculateTotalMock = sinon.stub(checkoutService, 'calculateTotal');
-            calculateTotalMock.throws(new Error('Produto não encontrado'));
-
-            const resposta = await request(app)
-                .post('/api/checkout')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    items: [
-                        {
-                            "productId": 13,
-                            "quantity": 2
-                        }
-                    ],
-                    freight: 0,
-                    paymentMethod: "boleto",
-                });
-
-            expect(resposta.status).to.equal(400);
-            expect(resposta.body).to.have.property('error', 'Produto não encontrado');
+        const testesDeErroDeNegocio = require('../fixture/requisicoes/checkout/postChekoutWithError.json');
+        testesDeErroDeNegocio.forEach(teste => {
+            it(`Testando a regra relacionada a ${teste.nomeDoTeste}`, async function () {
+                let calculateTotalMock, checkoutMock;
+                if (teste.postCheckout.paymentMethod === 'credit_card') {
+                    checkoutMock = sinon.stub(checkoutService, 'checkout');
+                    checkoutMock.throws(new Error('Dados do cartão obrigatórios para pagamento com cartão'));
+                } else if (teste.postCheckout.paymentMethod === 'boleto') {
+                    calculateTotalMock = sinon.stub(checkoutService, 'calculateTotal');
+                    calculateTotalMock.throws(new Error('Produto não encontrado'));
+                }
+                const resposta = await request(app)
+                    .post('/api/checkout')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(teste.postCheckout);
+                expect(resposta.status).to.equal(400);
+                expect(resposta.body).to.have.property('error', teste.mensagemEsperada);
+            });
         });
 
         afterEach(async () => {
